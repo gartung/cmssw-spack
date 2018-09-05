ebe69819301ce42991e8946b4a2a5dfe074cd773
Patrick Gartung <gartung@fnal.gov>
2018-09-05 11:41:32 -0500

    asciidoc is a build only wqdependency

--- packages/rivet/package.py
+++ packages/rivet/package.py
@@ -16,7 +16,7 @@ class Rivet(AutotoolsPackage):
     depends_on('yoda')
     depends_on('boost')
     depends_on('py-cython')
-    depends_on('asciidoc')
+    depends_on('asciidoc', type='build')
 
     def configure_args(self):
         args = ['--disable-silent-rules',
