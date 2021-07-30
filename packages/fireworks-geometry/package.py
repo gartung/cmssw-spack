4e84efa88404a81512d8a6a3b65d4b92c33370da
Patrick Gartung <gartung@fnal.gov>
2021-07-30 09:54:12 -0500

    Update fwlite

--- packages/fireworks-geometry/package.py
+++ packages/fireworks-geometry/package.py
@@ -5,11 +5,7 @@ import sys,os
 class FireworksGeometry(Package):
     url = "https://github.com/cms-data/Fireworks-Geometry/archive/V07-05-01.tar.gz"
 
-    version('07.05.01', '9f40fdf89286392d1d39d5ed52981051')
-    version('07.05.02', '0277dd37c0ff7664ea733445445efb6a')
-    version('07.05.03', 'cea2d9a9c03cb95470552f7fd73d3537')
-    version('07.05.04', 'ba4dd9a8eda665223e773c883cd367b2')
-
+    version('07-06-00', sha256='93312e7c60525c66c09c86fdc36db401c281b95ccb2d9195d735f84506f5868b')
 
     def install(self, spec, prefix):
         matches = []
--- packages/fwlite/package.py
+++ packages/fwlite/package.py
@@ -5,27 +5,37 @@ import re
 import os
 import fnmatch
 import sys
+import shutil
 
 
-class Fwlite(Package):
+class Fwlite(CMakePackage):
     """CMSSW FWLite built with cmake"""
 
     homepage = "http://cms-sw.github.io"
-    url = "https://github.com/cms-sw/cmssw/archive/CMSSW_9_0_0.tar.gz"
+    url = "https://github.com/gartung/fwlite/archive/refs/tags/11.3.1.2.tar.gz"
 
-    version('11.3.1', git='https://github.com/gartung/fwlite.git')
-    version('9.0.1', git='https://github.com/gartung/fwlite.git',
-            commit='6a9ace6', submodules=True)
-    version('9.2.6', git='https://github.com/gartung/fwlite.git',
-            commit='70f937c', submodules=True)
+    version('master', git='https://github.com/gartung/fwlite.git',tag='master')
+    version('11.3.1.2', sha256='0b293f6ad704faea3cc9e046542f1f86c9245ab30d39a2b54ba4d7423e6acfe6')
+
+    resource(name='cmaketools', git='https://github.com/gartung/cmaketools.git',
+              placement='cmaketools')
+    resource(name='upgrade-geometry', url='http://cmsrep.cern.ch/cmssw/download/Fireworks-Geometry/20200401/cmsGeom2026.root',
+              placement='data', sha256='3077e4d9abd62c57d1d71b30fa968ba52a7c12879a7fc71d90d94c4123e426fa', expand=False)
+    resource(name='geometry', url='https://github.com/cms-data/Fireworks-Geometry/archive/V07-06-00.tar.gz',
+              placement='data/Fireworks/Geometry/data', sha256='93312e7c60525c66c09c86fdc36db401c281b95ccb2d9195d735f84506f5868b')
+    resource(name='patcandidates', url='https://github.com/cms-data/DataFormats-PatCandidates/archive/V01-00-01.tar.gz',
+              placement='data/DataFormats/PatCandidates/data', sha256='5a0941df5a191d0f942e26838103659970ba29cb9cd4ab3d0cc45bcc01b408df')
+    resource(name='miniaod', url='https://cmsshow-rels.web.cern.ch/cmsShow-rels/samples/11_2/RelValZTTminiaod.root',
+              placement='data/samples/11_2/miniaod', sha256='4c4ddc418c7131f6eea16ea4bfefa36c2dba8ac2161640bf93b1d7889ce8fa2c', expand=False)
+    resource(name='aod', url='https://cmsshow-rels.web.cern.ch/cmsShow-rels/samples/11_2/RelVallZTTGenSimReco.root',
+              placement='data/samples/11_2/aod', sha256='209e6bda0496892d33137bf67ecb583b233e4c962154b6ca574aa33f824ea532', expand=False)
 
     if sys.platform != 'darwin':
         patch('patch')
     if sys.platform == 'darwin':
         depends_on('libuuid')
     depends_on('cmake', type='build')
-    depends_on('ninja', type='build')
-    depends_on('root')
+    depends_on('root+aqua+opengl~x~tbb')
     depends_on('intel-tbb-oneapi')
     depends_on('clhep')
     depends_on('md5-cms')
@@ -33,40 +43,64 @@ class Fwlite(Package):
     depends_on('vdt')
     depends_on('boost')
     depends_on('py-pybind11', type=('link', 'run', 'test'))
-    depends_on('xrootd')
     depends_on('hepmc')
     depends_on('pcre')
     depends_on('davix')
-    depends_on('libsigcpp')
+    depends_on('libsigcpp@2.10.3')
     depends_on('tinyxml2@6.2.0')
     depends_on('jpeg')
     depends_on('cppunit')
-    depends_on('fireworks-geometry')
     depends_on('xerces-c')
+    depends_on('fmt')
+    depends_on('eigen')
+    depends_on('openssl')
 
-    def install(self, spec, prefix):
-        with working_dir('spack-build', create=True):
-            options = ['../']
-            options.extend(std_cmake_args)
-            args = ['-DCMakeTools_DIR=%s/cmaketools' % self.stage.source_path,
-                    '-DCLHEP_ROOT_DIR=%s' % self.spec['clhep'].prefix,
-                    '-DBOOST_ROOT=%s' % self.spec['boost'].prefix,
-                    '-DTBB_ROOT_DIR=%s' % self.spec['intel-tbb-oneapi'].prefix,
-                    '-DMD5ROOT=%s' % self.spec['md5-cms'].prefix,
-                    '-DDAVIXROOT=%s' % self.spec['davix'].prefix,
-                    '-DSIGCPPROOT=%s' % self.spec['libsigcpp'].prefix,
-                    '-DSIGCPP_INCLUDE_DIR=%s/include/sigc++-2.0' % self.spec['libsigcpp'].prefix,
-                    '-DTINYXML2ROOT=%s' % self.spec['tinyxml2'].prefix,
-                    '-DCPPUNITROOT=%s' % self.spec['cppunit'].prefix,
-                    '-DXERCESC_ROOT_DIR=%s' % self.spec['xerces-c'].prefix]
-            options.extend(args)
-            if sys.platform == 'darwin':
-                options.append('-DUUID_INCLUDE_DIR=%s/include' %
-                               self.spec['libuuid'].prefix)
-                options.append('-DUUID_ROOT_DIR=%s' %
-                               self.spec['libuuid'].prefix)
-            options.append('-GNinja')
-            cmake(*options)
-            ninja = which('ninja')
-            ninja('-v', '-j4')
-            ninja('install')
+    def cmake_args(self):
+        cxxstd = self.spec['root'].variants['cxxstd'].value
+        args = ['-DCMakeTools_DIR=%s/cmaketools' % self.stage.source_path,
+                '-DCLHEP_ROOT_DIR=%s' % self.spec['clhep'].prefix,
+                '-DBOOST_ROOT=%s' % self.spec['boost'].prefix,
+                '-DTBB_ROOT_DIR=%s' % self.spec['intel-tbb-oneapi'].prefix,
+                '-DCMSMD5ROOT=%s' % self.spec['md5-cms'].prefix,
+                '-DDAVIXROOT=%s' % self.spec['davix'].prefix,
+                '-DSIGCPPROOT=%s' % self.spec['libsigcpp'].prefix,
+                '-DSIGCPP_INCLUDE_DIR=%s/sigc++-2.0' % self.spec['libsigcpp'].prefix.include,
+                '-DSIGCPP_LIB_INCLUDE_DIR=%s/sigc++-2.0/include' % self.spec['libsigcpp'].prefix.lib,
+                '-DTINYXML2ROOT=%s' % self.spec['tinyxml2'].prefix,
+                '-DCPPUNITROOT=%s' % self.spec['cppunit'].prefix,
+                '-DXERCESC_ROOT_DIR=%s' % self.spec['xerces-c'].prefix]
+        args.append('-DFMT_INCLUDE_DIR=%s' % self.spec['fmt'].prefix.include)
+        args.append('-DOPENSSL_INCLUDE_DIR=%s' % self.spec['openssl'].prefix.include)
+        args.append('-DEIGEN_INCLUDE_DIR=%s' % self.spec['eigen'].prefix.include)
+        args.append('-DVDT_ROOT_DIR=%s' % self.spec['vdt'].prefix)
+        args.append('-DCMAKE_CXX_STANDARD=%s' % cxxstd)
+        if sys.platform == 'darwin':
+            args.append('-DUUID_INCLUDE_DIR=%s/include' %
+                           self.spec['libuuid'].prefix)
+            args.append('-DUUID_ROOT_DIR=%s' %
+                           self.spec['libuuid'].prefix)
+        return(args)
+
+    @run_before('cmake')
+    def move_data(self):
+        cmssw_version = 'CMSSW.' + str(self.version[:-1])
+        cmssw_u_version = cmssw_version.replace('.', '_')
+        mkdirp(join_path(self.stage.source_path,'Fireworks/Core/data'))
+        with open(join_path(self.stage.source_path,'Fireworks/Core/data/version.txt'), 'w') as f:
+            f.write('%s' % cmssw_u_version)
+        shutil.move(join_path(self.stage.source_path, 'data', 'cmsGeom2026.root'), join_path(self.stage.source_path, 'data/Fireworks/Geometry/data', 'cmsGeom2026.root'))
+
+    @run_before('install')
+    def install_data(self):
+        install_tree(join_path(self.stage.source_path,'data'), join_path(self.prefix,'data'))
+
+    def setup_run_environment(self, env):
+        env.set('ROOT_INCLUDE_PATH', self.prefix.src)
+        env.set('CMSSW_RELEASE_BASE', self.prefix)
+        env.set('CMSSW_BASE', '%s' % self.prefix)
+        env.set('CMSSW_DATA_PATH', '%s/data' % self.prefix)
+        env.set('CMSSW_SEARCH_PATH', '%s/data/Geometry/Fireworks/data' % self.prefix)
+        if sys.platform == 'darwin':
+            env.set('DYLD_FALLBACK_LIBRARY_PATH', self.prefix.lib)
+        else: 
+            env.set('LD_LIBRARY_PATH', self.prefix.lib)
